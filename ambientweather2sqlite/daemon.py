fd276fc1bdd1268d3fa67de3bb580ac7c00d0c55
Harold Martin <harold.martin@gmail.com>
2025-06-14 13:18:17 -0700

    Save label metadata and pretty print to the command line

--- ambientweather2sqlite/daemon.py
+++ ambientweather2sqlite/daemon.py
@@ -1,19 +1,50 @@
+import json
 import sys
 import time
+from datetime import datetime
+from pathlib import Path
 
 from database import insert_observation
-from fetcher import fetch_live_data
+from fetcher import fetch_labels, fetch_live_data
 
 
-def start_daemon(live_data_url: str, database_path: str) -> None:
-    print(f"Starting to watch {live_data_url}")
+def create_metadata_if_not_exists(
+    database_path: str, live_data_url: str,
+) -> dict[str, str]:
+    path = Path(".".join(database_path.split(".")[:-1]) + "_metadata.json")
+    if not path.exists():
+        labels = fetch_labels(live_data_url)
+        path.write_text(json.dumps(labels, indent=4))
+    return json.loads(path.read_text())
+
+
+def clear_lines(n: int) -> None:
+    for _ in range(n):
+        print("\033[A\033[K", end="")
+
+
+def start_daemon(
+    live_data_url: str, database_path: str, period_seconds: int = 60,
+) -> None:
+    print(f"Observing {live_data_url}")
     print("Press Ctrl+C to stop")
+    metadata = create_metadata_if_not_exists(database_path, live_data_url)
+    remove_newlines = 0
     try:
         while True:
+            clear_lines(remove_newlines)
+            print(f"Updated at: {datetime.now()}")
             live_data = fetch_live_data(live_data_url)
-            print(live_data)
+            labeled_data = {
+                metadata.get(key, key): value for key, value in live_data.items()
+            }
+            pretty_data = json.dumps(labeled_data, indent=4)
+            remove_newlines = pretty_data.count("\n") + 2
+            print(pretty_data)
             insert_observation(database_path, live_data)
-            time.sleep(60)
+            for i in range(period_seconds, 0, -1):
+                print(f"Next update in {i} seconds", end="\r")
+                time.sleep(1)
     except KeyboardInterrupt:
         print(f"\nStopping... results saved to {database_path}")
         sys.exit(0)
