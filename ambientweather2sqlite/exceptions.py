8eab6c8f0740b0f79591ef1c211f23f8e6fdac75
Harold Martin <harold.martin@gmail.com>
2025-06-22 15:04:07 -0700

    Add /daily and /hourly server endpoints with flexible aggregation

--- ambientweather2sqlite/exceptions.py
+++ ambientweather2sqlite/exceptions.py
@@ -1,3 +1,33 @@
-class UnexpectedEmptyDictionaryError(Exception):
+class Aw2SqliteError(Exception):
+    pass
+
+
+class InvalidFormatError(Aw2SqliteError):
+    def __init__(self, field: str):
+        super().__init__(
+            f"Invalid aggregation field: {field}. Expected format: 'function_column'",
+        )
+
+
+class InvalidColumnNameError(Aw2SqliteError):
+    def __init__(self, column_name: str):
+        super().__init__(
+            f"Invalid column name: {column_name}.",
+        )
+
+
+class InvalidDateError(Aw2SqliteError):
+    def __init__(self, date: str):
+        super().__init__(f"Invalid date format: {date}. Expected YYYY-MM-DD")
+
+
+class MissingAggregationFieldsError(Aw2SqliteError):
+    def __init__(self):
+        super().__init__(
+            "At least one aggregation field must be provided e.g. /daily?q=avg_outHumi",
+        )
+
+
+class UnexpectedEmptyDictionaryError(Aw2SqliteError):
     def __init__(self):
         super().__init__("Dictionary is unexpectedly empty")
